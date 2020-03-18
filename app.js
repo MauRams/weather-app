d1fb2f80fc8b12983c88fb395370bc731a398da6
MauRams <maurice.ramsbottom@gmail.com>
2020-03-18 23:33:16 +0000

    added requests to darksky.net, begin API

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-console.log('Starting');
+const request = require('request')
+const chalk = require('chalk')
 
-setTimeout(() => {
-    console.log('2 second timer');
+const URL = 'https://api.darksky.net/forecast/8be10b9b369a21a2bb0f375d7ad25ea5/37.8267,-122.4233'
+
+request({ url: URL, json:true }, (error, response) => {
+    const temp = response.body.currently.temperature;
+    const precip = response.body.currently.precipProbability;
+    // console.log(response.body.currently.precipProbability);
+    console.log('it is currently ' + temp + ' degrees out. \nThere is a ' + chalk.red.bold(precip) + '% chance of rain');
     
-},2000) 
-
-setTimeout(() => {
-    console.log('0 second timer');
-
-}, 0)
-
-console.log('Stopping');
+    
+    
+    
+})

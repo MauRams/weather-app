c1a6716bc39c40103426cab3948dd84906b26f23
Maurice Ramsbottom <maurice.ramsbottom@gmail.com>
2020-03-19 23:03:30 +0000

    modifed API token, added weatehr call to darksky, added API to mapbox

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const request = require('request')
 const chalk = require('chalk')
+const getToken = require('./tokens.js')
 
-const URL = 'https://api.darksky.net/forecast/8be10b9b369a21a2bb0f375d7ad25ea5/37.8267,-122.4233'
+const wt = getToken.sendWKey().toString()
+const pt = getToken.sendMKey().toString()
 
-request({ url: URL, json:true }, (error, response) => {
-    const temp = response.body.currently.temperature;
-    const precip = response.body.currently.precipProbability;
-    // console.log(response.body.currently.precipProbability);
-    console.log('it is currently ' + temp + ' degrees out. \nThere is a ' + chalk.red.bold(precip) + '% chance of rain');
-    
-    
-    
-    
-})
+
+// get the cords from        the place name
+const placeReq = () => {
+    const placeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Dublin.json?access_token=' + pt + '&limit=1'
+    request({ url: placeURL, json:true }, (error, response) => {
+        const coords = response.body.features[0].geometry.coordinates;
+        return coords        
+    })
+}
+
+const weatherReq = () => {
+
+    const weatherURL = 'https://api.darksky.net/forecast/' + wt + '/37.8267,-122.4233?units=si'
+    request({ url: weatherURL, json: true }, (error, response) => {
+        const temp = response.body.currently.temperature;
+        const precip = response.body.currently.precipProbability;
+        console.log(response.body.daily.data[0].summary +
+            ' it is currently ' + temp + ' degrees out. \nThere is a ' +
+            chalk.red.bold(precip) + '% chance of rain');
+    })
+}
+
+weatherReq()
+
+// geodcoding

5c45098cfb5d99258b0130085b8597be03586e37
Maurice Ramsbottom <maurice.ramsbottom@gmail.com>
2020-03-21 00:37:13 +0000

    added error handling

--- app.js
+++ app.js
@@ -6,27 +6,52 @@ const wt = getToken.sendWKey().toString()
 const pt = getToken.sendMKey().toString()
 
 
-// get the cords from        the place name
+// geocoding
 const placeReq = () => {
     const placeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Dublin.json?access_token=' + pt + '&limit=1'
     request({ url: placeURL, json:true }, (error, response) => {
-        const coords = response.body.features[0].geometry.coordinates;
-        return coords        
+
+        if (error) {
+           console.log('Unable to connect to Location Service');
+            
+        } else if (response.body.features.length === 0){
+            console.log('Unable to find location, try another search');
+            
+        }else{
+            const latitude = response.body.features[0].center[1];
+            const longitude = response.body.features[0].center[0];
+
+            console.log(latitude, longitude);
+            // return latitude, longitude
+        }
+        
     })
 }
 
+//Weather Service
 const weatherReq = () => {
 
     const weatherURL = 'https://api.darksky.net/forecast/' + wt + '/37.8267,-122.4233?units=si'
     request({ url: weatherURL, json: true }, (error, response) => {
-        const temp = response.body.currently.temperature;
-        const precip = response.body.currently.precipProbability;
-        console.log(response.body.daily.data[0].summary +
-            ' it is currently ' + temp + ' degrees out. \nThere is a ' +
-            chalk.red.bold(precip) + '% chance of rain');
+
+        if (error) {
+            console.log('Unable to connect to weather service!');
+            
+        }else if(response.body.error){
+            console.log('Unable to find location')
+        } 
+        else {
+            const temp = response.body.currently.temperature;
+            const precip = response.body.currently.precipProbability;
+            console.log(response.body.daily.data[0].summary +
+                ' it is currently ' + temp + ' degrees out. \nThere is a ' +
+                chalk.red.bold(precip + '%') + ' chance of rain');
+        }
+        
+
+        
     })
 }
 
-weatherReq()
-
-// geodcoding
+// weatherReq()
+placeReq()

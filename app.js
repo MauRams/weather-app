2f832d1662b4b7a5bfc3b2371570221bd0525d3f
Maurice Ramsbottom <maurice.ramsbottom@gmail.com>
2020-03-21 15:43:44 +0000

    added better error handling

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const chalk = require('chalk')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
-// geocode('Dublin', (error, data) => {
-//     console.log('Error', error);
-//     console.log('Data', data);
-    
-// })
+const address = process.argv[2]
 
+geocode(address, (error, geoData) => {
+    if (!address) {
+        return console.log('Please provide a valid address');
 
+    } else {
+        if (error) {
+            return console.log(error);
 
-forecast(53.3425, -6.26583, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+        } else {
+            forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+
+                if (error) {
+                    return console.log(error)
+                } else {
+                    console.log(geoData.location);
+                    console.log(forecastData);
+                }
+            })
+        }
+    }
 })
